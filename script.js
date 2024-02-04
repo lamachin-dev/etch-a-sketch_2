8619838897c687c4fb68fab77282dff9ea53127f
lamachin-dev <56517440+lamachin-dev@users.noreply.github.com>
2024-02-04 13:42:27 -0800

    feat: add button to input # of squares

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
-const createGrid = function(){
+const createGrid = function(numSquares){
     const gridContainer = document.createElement("div");
-    gridContainer.style.width = "100px";
-    gridContainer.style.height = "100px";
+    gridContainer.classList.add("grid")
+    gridContainer.style.width = "400px";
+    gridContainer.style.height = "400px";
     gridContainer.style.border = "3px red solid";
     gridContainer.style.display = "flex";
     gridContainer.style.flexDirection = "column";
@@ -16,7 +17,7 @@ const createGrid = function(){
     )
 
     //Start with 4x4
-    let numberOfSquares = 4
+    let numberOfSquares = numSquares
 
   for(j=0; j < numberOfSquares; j++){
     const gridRow = document.createElement("div");
@@ -24,14 +25,12 @@ const createGrid = function(){
     gridRow.classList.add("row")
     gridRow.style.border = "1px solid black";
     gridRow.style.backgroundColor = "orange";
-    gridRow.style.minHeight = "10px";
     gridRow.style.display = "flex";
     gridRow.style.flex = 1
     //squares that go into the row
     for(i=0; i < numberOfSquares; i++){
         const gridSquare = document.createElement("div");
         gridSquare.style.border = "1px solid blue";
-        gridSquare.style.minWidth = "10px";
         gridSquare.style.flex = "1"
         gridSquare.classList.add("square")
 
@@ -46,7 +45,16 @@ const createGrid = function(){
   }  
 document.body.appendChild(gridContainer)
 
-
 }   
+createGrid(10)
 
-createGrid()
+const resetGrid = function(){
+    const numSquares = prompt("Enter # of squares per side")
+    const gridContainer = document.querySelector(".grid")
+
+    gridContainer.remove()
+    // document.body.removeChild(child)
+    createGrid(numSquares)
+    //TODO: add validation
+}
+
