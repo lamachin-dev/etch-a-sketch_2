70c2df3a8f50502e1ceff36c119404f39ab1e0fb
lamachin-dev <56517440+lamachin-dev@users.noreply.github.com>
2024-02-04 10:46:15 -0800

    feat: add dynamic column creation

--- script.js
+++ script.js
@@ -7,21 +7,24 @@ const createGrid = function(){
     //Start with 4x4
     let numberOfSquares = 4
 
-  for(i=0; i< numberOfSquares; i++){
+  for(j=0; j < numberOfSquares; j++){
     // const gridRowSquare = document.createElement(div)
     const gridRow = document.createElement("div");
     
+    gridRow.classList.add("row")
     gridRow.style.border = "1px solid black";
     gridRow.style.backgroundColor = "orange";
     gridRow.style.minHeight = "10px";
     gridRow.style.display = "flex";
     //squares that go into the row
-        //TODO:append squares to row
-            const gridSquare = document.createElement("div");
-            gridSquare.style.border = "1px solid black";
-            
-
-
+    //TODO:append squares to row
+    for(i=0; i < numberOfSquares; i++){
+        const gridSquare = document.createElement("div");
+        gridSquare.style.border = "1px solid blue";
+        gridSquare.style.minWidth = "10px";
+        gridSquare.style.flex = "1"
+        gridRow.appendChild(gridSquare)
+        }
 
     gridContainer.appendChild(gridRow)
   }  
