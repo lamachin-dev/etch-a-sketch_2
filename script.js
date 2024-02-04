478c969246e28caba9a6c6056f703b03f5af6eb6
lamachin-dev <56517440+lamachin-dev@users.noreply.github.com>
2024-02-04 12:17:56 -0800

    feat: square color change on mouseover

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ const createGrid = function(){
     gridContainer.style.border = "3px red solid";
     gridContainer.style.display = "flex";
     gridContainer.style.flexDirection = "column";
+
+    //Hover effect
+    gridContainer.addEventListener("mouseover", (e)=>{
+        let hoverTarget = e.target 
+        if (hoverTarget.class === "square") {
+            hoverTarget.style.backgroundColor = "red"}                                                  
+    }
+    )
+
     //Start with 4x4
     let numberOfSquares = 4
 
   for(j=0; j < numberOfSquares; j++){
-    // const gridRowSquare = document.createElement(div)
     const gridRow = document.createElement("div");
     
     gridRow.classList.add("row")
@@ -20,18 +28,25 @@ const createGrid = function(){
     gridRow.style.display = "flex";
     gridRow.style.flex = 1
     //squares that go into the row
-    //TODO:append squares to row
     for(i=0; i < numberOfSquares; i++){
         const gridSquare = document.createElement("div");
         gridSquare.style.border = "1px solid blue";
         gridSquare.style.minWidth = "10px";
         gridSquare.style.flex = "1"
+        gridSquare.classList.add("square")
+
+        gridSquare.addEventListener("mouseover", function(){
+            this.style.backgroundColor = "red"
+        })
+
         gridRow.appendChild(gridSquare)
         }
 
     gridContainer.appendChild(gridRow)
   }  
 document.body.appendChild(gridContainer)
+
+
 }   
 
 createGrid()
