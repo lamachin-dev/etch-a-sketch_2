22007f9eda9efd866bde8340776b4f1259766a5a
lamachin-dev <56517440+lamachin-dev@users.noreply.github.com>
2024-02-05 03:54:30 -0800

    fix: use hsl to determine initial square color

--- script.js
+++ script.js
@@ -7,22 +7,39 @@ const createGrid = function(numSquares){
     gridContainer.style.border = "3px red solid";
     gridContainer.style.display = "flex";
     gridContainer.style.flexDirection = "column";
+//TODO:
+//TODO:
+//TODO:
+//TODO:FIGURE out how to do RANDOMIZATIONand DARKENING at the same time
+//1. without counting (use HSL and have the formulas feed into themselves) 
+//2. with counting # of events fired
+//TODO:
 
     //Hover effect
     //Random RGB
-
+FIXME:gridContainer.style.backgroundColor
     gridContainer.addEventListener("mouseover", (e)=>{
         let hoverTarget = e.target;
         if (hoverTarget.className === "square") {
-            const randomRGBRed = Math.random() * 255
-            const randomRGBGreen = Math.random() * 255
-            const randomRGBBlue = Math.random() * 255
+            
+            //Read target square's HSL values
+            hoverTarget.style
+
+            let randomH = Math.random()
+
+        
+            //Add 10% more color every time the mouseover event occurs
+            randomRGBRed += (255/10 * (touchCount + 1))
+            randomRGBGreen += (255/10 * (touchCount + 1))
+            randomRGBBlue += (255/10 * (touchCount + 1))
+        
         
             hoverTarget.style.backgroundColor = 
             `rgb(${randomRGBRed}, ${randomRGBGreen}, ${randomRGBBlue})`
-            // hoverTarget.style.backgroundColor = 
-            // `red`
+            
+            touchcount++;
         }                                                  
+        
     }
     )
 
@@ -34,7 +51,7 @@ const createGrid = function(numSquares){
     
     gridRow.classList.add("row")
     gridRow.style.border = "1px solid black";
-    gridRow.style.backgroundColor = "orange";
+    gridRow.style.backgroundColor = "hsl(0,0%,50%)";
     gridRow.style.display = "flex";
     gridRow.style.flex = 1
     //squares that go into the row
